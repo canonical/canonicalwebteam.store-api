72035ce038d4c41f4a7bd5fedea760878cb08b19
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-06-23 15:39:59 +0100

    Added publisher API for charms

--- canonicalwebteam/store_api/stores/charmstore.py
+++ canonicalwebteam/store_api/stores/charmstore.py
@@ -3,8 +3,12 @@ from os import getenv
 import requests
 
 from canonicalwebteam.store_api.store import Store
+from canonicalwebteam.store_api.publisher import Publisher
 
-SNAPSTORE_API_URL = getenv("CHARMSTORE_API_URL", "https://api.snapcraft.io/")
+CHARMSTORE_API_URL = getenv("CHARMSTORE_API_URL", "https://api.snapcraft.io/")
+CHARMSTORE_PUBLISHER_API_URL = getenv(
+    "CHARMSTORE_PUBLISHER_API_URL", "https://api.snapcraft.io/"
+)
 
 
 class CharmStore(Store):
@@ -12,5 +16,37 @@ class CharmStore(Store):
         super().__init__(session, store)
 
         self.config = {
-            2: {"base_url": f"{SNAPSTORE_API_URL}v2/charms/"},
+            2: {"base_url": f"{CHARMSTORE_API_URL}v2/charms/"},
         }
+
+
+class CharmPublisher(Publisher):
+    def __init__(self, session=requests.Session()):
+        super().__init__(session)
+
+        self.config = {
+            1: {
+                "base_url": f"{CHARMSTORE_PUBLISHER_API_URL}publisher/api/v1/"
+            },
+        }
+
+    def _get_authorization_header(self, session):
+        return {"Cookie": session["publisher-macaroon"]}
+
+    def whoami(self, session):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=self.get_endpoint_url("whoami"), headers=headers
+        )
+
+        return self.process_response(response)
+
+    def get_account_packages(self, session):
+        headers = self._get_authorization_header(session)
+
+        response = self.session.get(
+            url=self.get_endpoint_url("charm"), headers=headers
+        )
+
+        return self.process_response(response)
