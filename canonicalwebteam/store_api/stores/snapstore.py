32957b27e3928fa7c7275f109ab2148f3eaec89c
Lukewh <luke@lukewh.com>
2023-07-12 09:32:25 +0100

    Remove "Prices" from snap fields

--- canonicalwebteam/store_api/stores/snapstore.py
+++ canonicalwebteam/store_api/stores/snapstore.py
@@ -44,7 +44,6 @@ class SnapStore(Store):
             "contact",
             "website",
             "publisher",
-            "prices",
             "media",
             "download",
             "version",
