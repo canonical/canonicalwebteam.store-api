22e617243061d89f6e0315f01352fdef307283e9
ALIMOT AKINBODE <halof.halima@gmail.com>
2024-03-27 14:48:12 +0300

    fix lint

--- tests/test_snapcraft_admin_api.py
+++ tests/test_snapcraft_admin_api.py
@@ -1,12 +1,10 @@
-
 from unittest.mock import Mock
 from os import getenv
-from pprint import pprint
-
 from vcr_unittest import VCRTestCase
-from canonicalwebteam.store_api.stores.snapstore import SnapStoreAdmin 
+from canonicalwebteam.store_api.stores.snapstore import SnapStoreAdmin
 
 test_session = getenv("DEVELOPER_TOKEN", {"developer_token": "secret"})
+
 
 class SnapPublisherTest(VCRTestCase):
 
@@ -15,7 +13,9 @@ class SnapPublisherTest(VCRTestCase):
         This removes the authorization header
         from VCR so we don't record auth parameters
         """
-        return {"filter_headers": ["Authorization", "Macaroon", "developer_token"]}
+        return {
+            "filter_headers": ["Authorization", "Macaroon", "developer_token"]
+        }
 
     def setUp(self):
         self.client = SnapStoreAdmin()
@@ -26,19 +26,13 @@ class SnapPublisherTest(VCRTestCase):
         self.client._get_publisherwg_authorization_header = Mock(
             return_value={"Authorization": "Macaroon some_random_macaroon"}
         )
-        
         mock__response = Mock()
         mock__response.status_code = 201
         mock_session.delete.return_value = mock__response
         payload = {
-                "packages": [
-                "12345"
-            ],
+            "packages": ["12345"],
         }
-        response = self.client.delete_featured_snaps(
-            mock_session, payload
-        )
-        pprint(response.json())
+        response = self.client.delete_featured_snaps(mock_session, payload)
         self.assertEqual(response.status_code, 201)
 
     def test_get_featured_snaps(self):
