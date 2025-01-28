67f7ee4310922bb5fb72d62c85670a163805f8b6
ALIMOT AKINBODE <halof.halima@gmail.com>
2025-01-28 11:50:30 +0300

    fix lint

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -1,5 +1,4 @@
 from os import getenv
-from pprint import pprint
 from vcr_unittest import VCRTestCase
 from canonicalwebteam.store_api.dashboard import Dashboard
 
@@ -34,22 +33,27 @@ class DashboardTest(VCRTestCase):
 
     def test_post_agreement(self):
         agreement = self.client.post_agreement(test_session, True)
+        self.assertIsInstance(agreement, dict)
 
     def test_post_username(self):
         username = self.client.post_username(test_session, "the-user")
         self.assertIsInstance(username, dict)
 
     def test_post_register_name_dispute(self):
-        response = self.client.post_register_name_dispute(test_session, "test-snap", "testing a dispute")
+        response = self.client.post_register_name_dispute(
+            test_session, "test-snap", "testing a dispute"
+        )
         self.assertIsInstance(response, dict)
         self.assertEqual(response["snap_name"], "test-snap")
 
     def test_get_snap_info(self):
         snap_info = self.client.get_snap_info(test_session, "steve-test-snap")
-        self.assertIsInstance(snap_info, dict)    
+        self.assertIsInstance(snap_info, dict)
 
     def test_get_package_upload_macaroon(self):
-        macaroon = self.client.get_package_upload_macaroon(test_session, "test-snap", ["testing"])
+        macaroon = self.client.get_package_upload_macaroon(
+            test_session, "test-snap", ["testing"]
+        )
         self.assertIn("macaroon", macaroon)
         self.assertIsInstance(macaroon["macaroon"], str)
 
@@ -58,29 +62,43 @@ class DashboardTest(VCRTestCase):
         self.assertIsInstance(snap_id, str)
 
     def test_get_snap_revision(self):
-        revision = self.client.get_snap_revision(test_session, "steve-test-snap", 22)
+        revision = self.client.get_snap_revision(
+            test_session, "steve-test-snap", 22
+        )
         self.assertIsInstance(revision, dict)
         self.assertIn("revision", revision)
 
     def test_snap_release_history(self):
-        release_history = self.client.snap_release_history(test_session, "steve-test-snap")
+        release_history = self.client.snap_release_history(
+            test_session, "steve-test-snap"
+        )
         self.assertIsInstance(release_history, dict)
         self.assertNotEqual(len(release_history), 0)
         self.assertIn("releases", release_history)
 
     def test_snap_channel_map(self):
-        channel_map = self.client.snap_channel_map(test_session, "steve-test-snap")
+        channel_map = self.client.snap_channel_map(
+            test_session, "steve-test-snap"
+        )
         self.assertIsInstance(channel_map, dict)
         self.assertNotEqual(len(channel_map), 0)
         self.assertIn("channel-map", channel_map)
         self.assertIsInstance(channel_map["channel-map"], list)
 
     def test_post_snap_release(self):
-        release = self.client.post_snap_release(test_session, "steve-test-snap", {"channels": ["edge"], "name": "steve-test-snap", "revision": 22})
+        release = self.client.post_snap_release(
+            test_session,
+            "steve-test-snap",
+            {"channels": ["edge"], "name": "steve-test-snap", "revision": 22},
+        )
         self.assertIsInstance(release, dict)
 
     def test_post_close_channel(self):
-        close_channel = self.client.post_close_channel(test_session, "bv9Q2i9CNAvTjt9wTx1cFC6SAT9YrEfG", {"channels": ["edge"]})
+        close_channel = self.client.post_close_channel(
+            test_session,
+            "bv9Q2i9CNAvTjt9wTx1cFC6SAT9YrEfG",
+            {"channels": ["edge"]},
+        )
         self.assertIsInstance(close_channel, dict)
         self.assertIn("channel_map_tree", close_channel)
 
@@ -100,14 +118,20 @@ class DashboardTest(VCRTestCase):
         self.assertIn("name", store)
 
     def test_get_store_snaps(self):
-        snaps = self.client.get_store_snaps(test_session, "testGahNg0wohVae8aY4")
+        snaps = self.client.get_store_snaps(
+            test_session, "testGahNg0wohVae8aY4"
+        )
         self.assertIsInstance(snaps, list)
 
     def test_get_store_members(self):
-        members = self.client.get_store_members(test_session, "testGahNg0wohVae8aY4")
+        members = self.client.get_store_members(
+            test_session, "testGahNg0wohVae8aY4"
+        )
         self.assertIsInstance(members, list)
         self.assertNotEqual(len(members), 0)
 
     def test_get_store_invites(self):
-        invite = self.client.get_store_invites(test_session, "testGahNg0wohVae8aY4")
+        invite = self.client.get_store_invites(
+            test_session, "testGahNg0wohVae8aY4"
+        )
         self.assertIsInstance(invite, list)
